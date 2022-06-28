5cb0c48a0c04f73b3e7afdd3df58047123c594b3
rawift <102667382+rawift@users.noreply.github.com>
2022-06-28 23:52:55 +0530

    Add files via upload

---
+++ config/nodemailer.js
@@ -0,0 +1,37 @@
+const nodemailer = require("nodemailer");
+const ejs = require('ejs');
+const path = require('path')
+
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: false,
+    auth: {
+        user: 'rawift@gmail.com',
+        pass: 'ofxfexlilkpvgmde'
+    }
+});
+
+
+let renderTemplate = (data, relativePath) => {
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname, '../views/mailers', relativePath),
+        data,
+        function(err, template){
+         if (err){console.log('error in rendering template', err); return}
+         
+         mailHTML = template;
+        }
+    )
+
+    return mailHTML;
+}
+
+
+module.exports = {
+    transporter: transporter,
+    renderTemplate: renderTemplate
+}
---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,44 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const crypto = require('crypto');
+const User = require('../models/user');
+
+
+// tell passport to use a new strategy for google login
+passport.use(new googleStrategy({
+        clientID: "678000404310-j317io16csn35rmh3lpb6ls8b3coctur.apps.googleusercontent.com",
+        clientSecret: "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",
+        callbackURL: "http://localhost:8000/users/auth/google/callback",
+    },
+
+    function(accessToken, refreshToken, profile, done){
+        // find a user
+        User.findOne({email: profile.emails[0].value}).exec(function(err, user){
+            if (err){console.log('error in google strategy-passport', err); return;}
+            console.log(accessToken, refreshToken);
+            console.log(profile);
+
+            if (user){
+                // if found, set this user as req.user
+                return done(null, user);
+            }else{
+                // if not found, create the user and set it as req.user
+                User.create({
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    password: crypto.randomBytes(20).toString('hex')
+                }, function(err, user){
+                    if (err){console.log('error in creating user google strategy-passport', err); return;}
+
+                    return done(null, user);
+                });
+            }
+
+        }); 
+    }
+
+
+));
+
+
+module.exports = passport;
